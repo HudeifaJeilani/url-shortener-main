e0d07d8339cd4ca761180e786c6d90966d44cdb4
hjhudz <hudeifa.jeilani@hotmail.com>
2026-04-17 20:03:32 +0100

    add WAF web ACL with AWS managed common rule set

--- terraform/main.tf
+++ terraform/main.tf
@@ -365,3 +365,48 @@ resource "aws_vpc_endpoint" "sqs" {
   })
 }
 
+
+
+
+
+resource "aws_wafv2_web_acl" "main" {
+  name  = "${local.prefix}-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.prefix}-common-rules"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${local.prefix}-waf"
+    sampled_requests_enabled   = true
+  }
+
+  tags = local.common_tags
+}
+
+
+
